10129d66f1b624dbad0f4c83b123526567804b0b
Judit Camps Bruch <juditcampsbruch@MacBook-MacBook-Pro-de-Judit.local>
2023-09-01 11:01:03 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Javascript
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } 
+    });
+});
+
+
+const hiddenElement = document.querySelectorAll(".hidden")
+hiddenElement.forEach((el) => observer.observe(el));
+
+
+
+
+
+
+
