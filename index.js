0d174608576c84a084661eb07da802bddd9bc820
Judit Camps Bruch <juditcampsbruch@MacBook-MacBook-Pro-de-Judit.local>
2023-09-06 10:34:57 +0200

    index.js file modified

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Javascript
 
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -13,9 +12,12 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElement = document.querySelectorAll(".hidden")
 hiddenElement.forEach((el) => observer.observe(el));
 
-
-
-
-
+if (history.scrollRestoration) {
+    history.scrollRestoration = "manual";
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
 
 
